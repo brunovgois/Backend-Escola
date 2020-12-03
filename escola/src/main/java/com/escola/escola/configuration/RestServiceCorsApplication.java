8fc3e1075bf57b989d01de9fb1ddbeed4ea4a63f
Bruno Gois <brunovgois@gmail.com>
2020-12-03 18:27:33 -0300

    Configurar CORS

---
+++ escola/src/main/java/com/escola/escola/configuration/RestServiceCorsApplication.java
@@ -0,0 +1,13 @@
+package com.escola.escola.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class RestServiceCorsApplication implements WebMvcConfigurer{
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*");
+    }
+}
