565b3a10525690c6eb55280164a6548ee48b78cd
Bruno Gois <brunovgois@gmail.com>
2020-12-15 23:25:59 -0300

    Alterar CORS para incluir todos os metodos usados no CRUD

--- escola/src/main/java/com/escola/escola/configuration/RestServiceCorsApplication.java
+++ escola/src/main/java/com/escola/escola/configuration/RestServiceCorsApplication.java
@@ -2,12 +2,14 @@ package com.escola.escola.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class RestServiceCorsApplication implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*");
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET","POST","PUT","DELETE");
     }
 }
